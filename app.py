b003947732bfc7f30745a7c61d940d9624485ecd
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2019-04-21 11:29:34 +0800

    use gunicorn to deploy

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+from datetime import datetime
+
 from flask import Flask
-from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello, World!"
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(debug=True, use_reloader=True)
